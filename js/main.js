c5d033bc6b0c02c56f0f29ba71810767223a68d3
saikrishnachowdary <57797731+saikrishnachowdary@users.noreply.github.com>
2020-05-06 10:05:37 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,17 +9,12 @@ var username;
 var pcConfig = {
   iceServers: [
     {
-      urls: "stun:stun.l.google.com:19302"
+     "url": "stun:stun.immusservices.com:5349"
     },
     {
-      urls: "turn:192.158.29.39:3478?transport=udp",
-      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-      username: "28224511:1379330808"
-    },
-    {
-      urls: "turn:192.158.29.39:3478?transport=tcp",
-      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-      username: "28224511:1379330808"
+      'url': 'turn:turn.immusservices.com:5349',
+        'credential': '123456',
+        'username': 'shiva'
     }
   ]
 };
