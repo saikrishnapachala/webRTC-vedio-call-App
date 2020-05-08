4966709733e7b0ba5b4a51a52f4f8cd01dfd5548
saikrishnachowdary <57797731+saikrishnachowdary@users.noreply.github.com>
2020-05-08 11:01:43 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,9 +6,26 @@ var localVideo = document.querySelector("#localVideo");
 var remoteVideo = document.querySelector("#remoteVideo");
 var callBtn = document.getElementById("btnCall");
 var username;
+var pcConfig = {
+  iceServers: [
+    {
+      urls: "stun:stun.l.google.com:19302"
+    },
+    {
+      urls: "turn:192.158.29.39:3478?transport=udp",
+      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+      username: "28224511:1379330808"
+    },
+    {
+      urls: "turn:192.158.29.39:3478?transport=tcp",
+      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+      username: "28224511:1379330808"
+    }
+  ]
+};
 
 function createPeerConnection() {
-  peerConnection = new RTCPeerConnection();
+  peerConnection = new RTCPeerConnection(pcConfig);
   peerConnection.onicecandidate = function(event) {
     if (event.candidate) {
       // send local ICE node to remote
